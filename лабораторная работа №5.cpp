dcf84835b4a0aa6e0e335f796f7cb5a6e9cb3f91
Abdibaitov <91374399+Abdibaitov@users.noreply.github.com>
2021-12-15 00:03:53 +0600

    Add files via upload

---
+++ лабораторная работа №5.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+    using namespace std;
+    int a, b, s;
+    cout << "a=";
+    cin >> a;
+    cout << "b=";
+    cin >> b;
+    s = a + b;
+    cout << "S=" << s << endl;
+    std::cout << "Hello World!\n";
+}
